4fe672ead331c9f938e88d3a0faed6827e05d9e8
Nathan da Silva Barrozo <email.do.natann@gmail.com>
2023-09-10 17:52:34 -0300

    Meu projeto conta banco

---
+++ ContaTerminal/src/ContaTerminal.java
@@ -0,0 +1,11 @@
+public class ContaTerminal {
+    
+    int numero;
+    String agencia;
+    String nomeDoCliente;
+    float saldo;
+
+    public static void main (String[] args) throws Exception{
+        System.out.println("oi");
+    }
+}
